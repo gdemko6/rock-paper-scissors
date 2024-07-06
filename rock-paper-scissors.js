7e216a475cfa39cf402b0809da19a2c02b7e71cf
Greg Demko <gregory.demko@my.uwrf.edu>
2024-07-05 20:51:10 -0500

    make methods to get human and computer choice of rock paper or scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,22 @@
 let humanScore = 0;
 let computerScore = 0;
 
+function getComputerChoice() {
+    let computerChoice;
+    let randNum = Math.floor(Math.random()*3)+1;
+    if (randNum == 1){
+        computerChoice="rock";
+    }
+    else if (randNum == 2){
+        computerChoice="paper";
+    }
+    else if (randNum == 3){
+        computerChoice="scissors";
+    }
+    return computerChoice;
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, paper, or scissors?");
+    return humanChoice;
+}
